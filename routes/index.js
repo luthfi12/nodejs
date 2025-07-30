a1a921f22a6aab62f1613e2ea3591a15d0e08c89
Luthfi azmi <luthfiazmi120201@gmail.com>
2025-07-30 08:28:27 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,34 @@
-const express = require('express');
-const path = require('path');
-const router = express.Router();
+// index.js
+const express = require("express");
+const fetch = require("node-fetch");
+const app = express();
+app.use(express.json());
 
-// Serve the index.html file for the root route
-router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../views/index.html'));
+app.post("/track", async (req, res) => {
+  const { platNomor, biaya } = req.body;
+
+  const params = new URLSearchParams({
+    idsite: "2",                             
+    rec: "1",
+    e_c: platNomor,                          // Kategori = plat nomor
+    e_a: "naik",                             // Action = naik
+    e_n: "event-naik",                       // Event name
+    e_v: biaya,                              // Value = biaya
+    apiv: "1",
+    rand: Math.random().toString()
+  });
+
+  try {
+    await fetch("http://192.168.1.2/matomo/index.php" + params.toString());
+    res.send("Event dikirim ke Matomo");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Gagal kirim event ke Matomo");
+  }
 });
 
-module.exports = router;
+app.get("/", (req, res) => {
+  res.send("Proxy Matomo Aktif");
+});
+
+app.listen(process.env.PORT || 3000);
